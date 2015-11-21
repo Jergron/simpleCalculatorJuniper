e7272ad11950a4b06e1c5aa5bb2bd9c8fed72738
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-20 20:51:43 -0600

    Created AddSpace method using regex

---
+++ SimpleCalculator/Parse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SimpleCalculator
+{
+    public class Parse
+    {
+        public string Expression { get; set; }
+
+        public string AddSpace(string input)
+        {
+            string operators = @"[+-/%*]";
+            string pattern = @"\s*[+-/%*]\s*";
+            string replacement = " " + Regex.Match(input, operators) + " ";
+            Regex rgx = new Regex(pattern);
+            return rgx.Replace(input, replacement);
+
+        }
+    }
+}
