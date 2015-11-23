aa3738bb6e33073a78290d60bb0377d10061db94
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-22 19:12:22 -0600

    Refactored regex patterns to be more strict with users input

--- SimpleCalculator/Parse.cs
+++ SimpleCalculator/Parse.cs
@@ -2,17 +2,18 @@
 using System.Text.RegularExpressions;
 
 namespace SimpleCalculator
-{
+{ 
     public class Parse
     {
+        
         public string Expression { get; set; }
 
         public string AddSpace(string input)
         {
-            string firstDigit = @"^\d+";
-            string secondDigit = @"\d*\D*$";
+            string firstDigit = @"^-?\d+";
+            string secondDigit = @"\d*\d$";
             string operators = @"[+-/%*]";
-            string pattern = @"\d+\s{0,0}[+-/%*]\s{0,0}\d+";
+            string pattern = @"-?\d+\s{0,0}[+-/%*]\s{0,0}-?\d+";
             string replacement = Regex.Match(input, firstDigit) + " " + Regex.Match(input, operators) + " " + Regex.Match(input, secondDigit);
             Regex rgx = new Regex(pattern);
             if (rgx.IsMatch(input))
@@ -25,11 +26,12 @@ namespace SimpleCalculator
 
         public string ValEx(string input)
         {
-           string newInput = AddSpace(input);
-            Regex rgx = new Regex(@"-?\d+\s[+-/*%]{1,1}\s-?\d");
-            if (rgx.IsMatch(newInput))
+            string expression = AddSpace(input);
+
+            Regex rgx = new Regex(@"-?\d+\s[+-/*%]{1,1}\s-?\d+");
+            if (rgx.IsMatch(expression))
             {
-                return newInput;
+                return expression;
             }
             throw new ArgumentException();
         }
