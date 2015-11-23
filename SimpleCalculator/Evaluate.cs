eab29febe74839083ba2f155785fcf8b2c2b17e1
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-22 19:10:18 -0600

    Implemented EvalEx method to evaluate expression from users input from parse class

---
+++ SimpleCalculator/Evaluate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Evaluate : Parse
+    {
+        int x;
+        int y;
+
+        public int EvalEx(string input)
+        {
+            string[] array = input.Split(' ');
+            for (int i = 0; i < array.Length; i++)
+            {
+                int.TryParse(array[0], out x);
+                int.TryParse(array[2], out y);
+               
+                if (array[1] == "+")
+                {
+                    return x + y;
+                }
+                else if (array[1] == "-")
+                {
+                    return x - y;
+                }
+                else if (array[1] == "*")
+                {
+                    return x * y;
+                }
+                else if (array[1] == "/")
+                {
+                    return x / y;
+                }
+                else 
+                {
+                    return x % y;
+                }
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
