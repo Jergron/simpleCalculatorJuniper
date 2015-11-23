270b45b7b831e6a500142ad5580528ac914f8bb6
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-22 22:29:16 -0600

    Created stack class and implemented tests to handle expressions and evaluated expressions

---
+++ SimpleCalculator/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace SimpleCalculator
+{
+    public class Stack : Evaluate 
+    {
+        public string Expression
+        {
+            get { return lastq; }
+            set { lastq = value; }
+        }
+        public int Answer
+        {
+            get { return last; }
+            set { last = value; }
+        }
+        //public void CheckConstants()
+        //{
+        //    Regex rgx = new Regex(@"[a-zA-Z]{1,1}\s[+-*/%]{1,1}\s[a-zA-Z]{1,1}");
+
+        //    if (rgx.IsMatch(Expression))
+        //    {
+        //         Expression = Expression;
+        //    }
+        //}
+       
+    }
+}
---
+++ SimpleCalculatorTests/StackTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using SimpleCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void StackEnsureICanCreateInstance()
+        {
+            Stack userInput = new Stack();
+            Assert.IsNotNull(userInput);
+        }
+
+        [TestMethod]
+        public void StackEnsureLastEvaluatedExpressionIsSet()
+        {
+            // Arrange
+            Stack user = new Stack();
+            Parse input = new Parse();
+            // Act
+            user.EvalEx(input.ValEx("1+1"));
+            string actual = user.Expression;
+            string expected = "1 + 1";
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StackEnsureLastAnswerIsSet()
+        {
+            // Arrange
+            Stack user = new Stack();
+            Parse input = new Parse();
+            // Act
+            user.EvalEx(input.ValEx("1+1"));
+            int actual = user.Answer;
+            int expected = 2;
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StackProveLastQHandlesCommandInEvaluateClass()
+        {
+            // Arrange
+            Stack user = new Stack();
+            // Act  
+            user.EvalEx(user.ValEx("1+1"));
+            string actual = user.lastq;
+            string expected = "1 + 1";
+            // Assert 
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StackProveLastHandlesCommandInEvaluateClass()
+        {
+            // Arrange
+            Stack user = new Stack();
+            // Act  
+            user.EvalEx(user.ValEx("1+1"));
+            int actual = user.last;
+            int expected = 2;
+            // Assert 
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
