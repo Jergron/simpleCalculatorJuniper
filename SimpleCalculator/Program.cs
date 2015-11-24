3e4afd667cef435e0a4b25bdd0762969ff124698
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-23 19:26:01 -0600

    Implemented stack class and methods in program class to be use in console

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SimpleCalculator
 {
@@ -10,6 +7,41 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            Stack user = new Stack();
+            Regex rgx = new Regex(@"-?\d+\s?[+-/*%]\s?-?\d+");
+            int counter = 0;
+            bool running = true;
+
+            while (running)
+            {
+               Console.Write("[" + counter + "]> ");
+                string input = Console.ReadLine();
+
+                if( input == "exit" || input =="quit")
+                {
+                    Console.WriteLine("Bye!!");
+                    running = false;
+                }
+                else if(input == "lastq")
+                {                 
+                    Console.WriteLine("> Your last expression was: " + "\" " + user.Expression + " \"");      
+                }
+                else if (input == "last")
+                {
+                    Console.WriteLine("> Your last answer was: " + "\" " + user.Answer + " \"");
+                }
+                else if(!rgx.IsMatch(input))
+                {
+                    Console.WriteLine("> Please enter a valid expression or command");
+                    counter -= 1;
+                }
+                else
+                {
+                    user.EvalEx(input);
+                    Console.WriteLine("   = " + user.Answer);
+                }
+                counter += 1;
+            }   
         }
     }
 }
