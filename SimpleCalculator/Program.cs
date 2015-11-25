6a2e9d1eb8d6a0b6db05b29888c51f7aae8d48b8
Jeremy Grondahl <jeremygrondahl@gmail.com>
2015-11-24 19:47:30 -0600

    Implemented conditional staments to handle dictionary values for user input

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -3,15 +3,21 @@ using System.Text.RegularExpressions;
 
 namespace SimpleCalculator
 {
-    class Program
+    class Program 
     {
+  
         static void Main(string[] args)
         {
             Stack user = new Stack();
+            Program count = new Program();
             Regex rgx = new Regex(@"-?\d+\s?[+-/*%]\s?-?\d+");
+            Regex addToDictionary = new Regex(@"[a-zA-Z]\s?[=]\s?-?\d+");
+            Regex constants1 = new Regex(@"[a-zA-Z]\s?[+-/*%]\s?\d+");
+            Regex constants2 = new Regex(@"\d+\s?[+-/*%]\s?[a-zA-Z]");
+
             int counter = 0;
             bool running = true;
-
+            
             while (running)
             {
                Console.Write("[" + counter + "]> ");
@@ -30,15 +36,75 @@ namespace SimpleCalculator
                 {
                     Console.WriteLine("> Your last answer was: " + "\" " + user.Answer + " \"");
                 }
-                else if(!rgx.IsMatch(input))
+                else if (addToDictionary.IsMatch(input))
                 {
-                    Console.WriteLine("> Please enter a valid expression or command");
-                    counter -= 1;
+                    user.Constants(input);
                 }
-                else
+                else if (constants1.IsMatch(input))
+                {
+                    string newInput = user.AddSpace(input);
+                    string[] array = newInput.Split();
+                    int value = user.constants[array[0]];
+
+                    if(array[1] == "+")
+                    {
+                        user.EvalEx(value + "+" + array[2]);
+                    }
+                    else if(array[1] == "-")
+                    {
+                        user.EvalEx(value + "-" + array[2]);
+                    }
+                    else if (array[1] == "/")
+                    {
+                        user.EvalEx(value + "/" + array[2]);
+                    }
+                    else if (array[1] == "*")
+                    {
+                        user.EvalEx(value + "*" + array[2]);
+                    }
+                    else 
+                    {
+                        user.EvalEx(value + "%" + array[2]);
+                    }
+                    Console.WriteLine("   = " + user.Answer);
+                }
+                else if (constants2.IsMatch(input))
+                {
+                    string newInput = user.AddSpace(input);
+                    string[] array = newInput.Split();
+                    int value = user.constants[array[2]];
+
+                    if (array[1] == "+")
+                    {
+                        user.EvalEx(array[0] + "+" + value);
+                    }
+                    else if (array[1] == "-")
+                    {
+                        user.EvalEx(array[0] + "-" + value);
+                    }
+                    else if (array[1] == "/")
+                    {
+                        user.EvalEx(array[0] + "/" + value);
+                    }
+                    else if (array[1] == "*")
+                    {
+                        user.EvalEx(array[0] + "*" + value);
+                    }
+                    else
+                    {
+                        user.EvalEx(array[0] + "%" + value);
+                    }
+                    Console.WriteLine("   = " + user.Answer);
+                }
+                else if(rgx.IsMatch(input))
                 {
                     user.EvalEx(input);
                     Console.WriteLine("   = " + user.Answer);
+                }
+                else
+                {
+                    Console.WriteLine("> Please enter a valid expression or command");
+                    counter -= 1;
                 }
                 counter += 1;
             }   
